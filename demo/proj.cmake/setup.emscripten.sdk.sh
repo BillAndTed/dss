2414871acb25deac1dab6a2da5c061aa7edf2810
johnoneil <oneil.john@gmail.com>
2019-07-31 23:49:56 -0700

    Improved emsdk setup script.

--- demo/proj.cmake/setup.emscripten.sdk.sh
+++ demo/proj.cmake/setup.emscripten.sdk.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -e
 
+# This script is an aid/document to help install emscripten build tools on your system
+# Alternate instructions can be found here: https://emscripten.org/docs/getting_started/downloads.html
+# But I've found something like this script is all you need.
+# To run, specify an environment variable EMSDK_INSTALL_DIR where you want the emsdk installed,
+# and source this script to pick up new paths/env variables
+#
+# example:
+# 'EMSDK_INSTALL_DIR=~/myinstalldir source ./setup.emscripten.sdk.sh'
+#
+# After running this script you should have access to tools such as 'emcc' and 'em++'
+# It's suggested that rather than running this script every time you need those tools, you
+# instead source the script ${EMSDK_INSTALL_DIR}/emsdk-portable/emsdk_env.sh in your
+# .bashrc or .bash_profile or similar.
+
 WGET=wget
-EMSDK_INSTALL_DIR=~/code
+EMSDK_INSTALL_DIR=${EMSDK_INSTALL_DIR:-${HOME}/emscripten}
 EMSDK_PORTABLE=emsdk-portable.tar.gz
 EMSDK_PORTABLE_URL=https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
 
@@ -14,15 +28,20 @@ EMSDK_PORTABLE_URL=https://s3.amazonaws.com/mozilla-games/emscripten/releases/em
 # Install Java (optional, only needed for Closure Compiler minification)
 #${INSTALL} default-jre
 
-# download and install the emscripten 'emsdk'
+# download the 'emsdk' if necessary
+echo "EMSDK will reside at: ${EMSDK_INSTALL_DIR}"
+mkdir -p ${EMSDK_INSTALL_DIR}
 pushd ${EMSDK_INSTALL_DIR}
 if [ ! -f ${EMSDK_PORTABLE} ]; then
     ${WGET} ${EMSDK_PORTABLE_URL}
 fi
 
-tar -xvf ${EMSDK_PORTABLE}
-cd emsdk-portable
+# decompress the emsdk if necessary
+if [ ! -f emsdk-portable ]; then
+    tar -xvf ${EMSDK_PORTABLE}
+fi
 
+cd emsdk-portable
 
 # Fetch the latest registry of available tools.
 ./emsdk update
@@ -37,3 +56,9 @@ source ./emsdk_env.sh
 
 # TODO: probably best to add installed emsdk dir tools to PATH
 # TODO: add 'emsdk activate' to .bashrc
+
+echo "###########################################################################"
+echo "Emscripten EMSDK present at ${EMSDK_INSTALL_DIR}"
+echo "You should now have access to emscripten tools like emcc, em++, emmake etc."
+echo "You may wish to source ${EMSDK_INSTALL_DIR}/emsdk-portable/emsdk_env.sh in your .bashrc or .bash_profile or similar."
+echo "###########################################################################"

d238c9ec96232821eef7b3041a4673696e182db8
johnoneil <oneil.john@gmail.com>
2019-08-08 16:41:47 -0700

    Improved valgrind memory leak checks on linux

--- demo/proj.cmake/run.memory.usage.linux.sh
+++ demo/proj.cmake/run.memory.usage.linux.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env sh
 set -e
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 TARGET=demo
 BUILD=${BUILD:-Debug}
 BUILD_DIR=build/${BUILD}
 
 #tools
-export VALGRIND=valgrind
-VALGRIND_OPTIONS="--tool=memcheck --leak-check=full"
+VALGRIND=valgrind
+VALGRIND_OPTIONS="--tool=memcheck"
+
+# For full verbose leak checks run with option:
+VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --leak-check=full"
+
+# To run with valgrind false positives turned off (usually X11 related on linux)
+# run with option:
+VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --suppressions=${SCRIPTPATH}/linux/valgrind.suppressions"
+
+# To generate a suppression file (which can quieten valgrind false positives)
+# run with option:
+#VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --gen-suppressions=all"
+
+echo "Running with valgrind options: ${VALGRIND_OPTIONS}"
 
 # Bail with error message if we don't have valgrind installed.
 which ${VALGRIND} || echo "Could not find ${VALGRIND} on this system. Is it installed?"
